568b7055d154950242798058256e8519b6270bec
Võ Hoàng Minh <minh7n3@gmail.com>
2025-05-24 15:11:05 +0700

    initial commit

---
+++ src/main/java/com/Swp_391_gr7/smoking_cessation_support_platform_backend/SmokingCessationSupportPlatformBackendApplication.java
@@ -0,0 +1,13 @@
+package com.Swp_391_gr7.smoking_cessation_support_platform_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmokingCessationSupportPlatformBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmokingCessationSupportPlatformBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Swp_391_gr7/smoking_cessation_support_platform_backend/SmokingCessationSupportPlatformBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Swp_391_gr7.smoking_cessation_support_platform_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmokingCessationSupportPlatformBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
