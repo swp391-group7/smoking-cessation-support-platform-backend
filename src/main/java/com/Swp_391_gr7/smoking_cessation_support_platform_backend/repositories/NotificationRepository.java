b50a215322aa8eb2145d8f9c280ec2056f2d94ad
LTdoshit <tuannleeeee@gmail.com>
2025-06-07 17:43:03 +0700

    overkill
    we are using a machete to kill an ant, this is overkilling the project

--- src/main/java/com/Swp_391_gr7/smoking_cessation_support_platform_backend/repositories/NotificationRepository.java
+++ src/main/java/com/Swp_391_gr7/smoking_cessation_support_platform_backend/repositories/NotificationRepository.java
@@ -1,4 +1,13 @@
 package com.Swp_391_gr7.smoking_cessation_support_platform_backend.repositories;
 
-public interface NotificationRepository {
+import com.Swp_391_gr7.smoking_cessation_support_platform_backend.models.entity.Notification;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Repository
+public interface NotificationRepository extends JpaRepository<Notification, Long> {
+    List<Notification> findByDeliveredFalseAndCreatedAtBefore(LocalDateTime cutoff);
 }
---
+++ src/main/java/com/Swp_391_gr7/smoking_cessation_support_platform_backend/services/DailyLogService.java
@@ -0,0 +1,4 @@
+package com.Swp_391_gr7.smoking_cessation_support_platform_backend.services;
+
+public class DailyLogService {
+}
