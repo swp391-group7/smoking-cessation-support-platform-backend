3643ec8063516da1b094cf6548c441402616d218
Võ Hoàng Minh <minh7n3@gmail.com>
2025-06-18 02:23:47 +0700

    update testcase

---
+++ src/test/java/com/Swp_391_gr7/smoking_cessation_support_platform_backend/SmokingCessationSupportPlatformBackendApplicationTests.java
@@ -0,0 +1,15 @@
+package com.Swp_391_gr7.smoking_cessation_support_platform_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class SmokingCessationSupportPlatformBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
