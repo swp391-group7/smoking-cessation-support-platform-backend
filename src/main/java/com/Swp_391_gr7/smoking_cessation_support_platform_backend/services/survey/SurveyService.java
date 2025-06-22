a3d53e5208e539032a03329d0eecf48521519d17
Võ Hoàng Minh <minh7n3@gmail.com>
2025-06-22 13:52:50 +0700

    add SurveyService

--- src/main/java/com/Swp_391_gr7/smoking_cessation_support_platform_backend/services/survey/SurveyService.java
+++ src/main/java/com/Swp_391_gr7/smoking_cessation_support_platform_backend/services/survey/SurveyService.java
@@ -1,4 +1,14 @@
 package com.Swp_391_gr7.smoking_cessation_support_platform_backend.services.survey;
 
+import com.Swp_391_gr7.smoking_cessation_support_platform_backend.models.dto.survey.CreateSurveyRequest;
+import com.Swp_391_gr7.smoking_cessation_support_platform_backend.models.dto.survey.SurveyDto;
+
+import java.util.UUID;
+
 public interface SurveyService {
+    SurveyDto createSurvey(UUID userId, CreateSurveyRequest request);
+    SurveyDto getSurvey(UUID userId);
+    SurveyDto updateSurvey(UUID userId, CreateSurveyRequest request);
+    void deleteSurvey(UUID userId);
+    SurveyDto getSurveyById(UUID surveyId);
 }
