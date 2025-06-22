5cf515d2b34695ab6fa9d434e79a15e927a7b44a
Võ Hoàng Minh <minh7n3@gmail.com>
2025-06-23 03:47:40 +0700

    update UpdateAnswerRequest

--- src/main/java/com/Swp_391_gr7/smoking_cessation_support_platform_backend/models/dto/answer/UpdateAnswerRequest.java
+++ src/main/java/com/Swp_391_gr7/smoking_cessation_support_platform_backend/models/dto/answer/UpdateAnswerRequest.java
@@ -1,4 +1,7 @@
 package com.Swp_391_gr7.smoking_cessation_support_platform_backend.models.dto.answer;
 
+import java.time.LocalDateTime;
+
 public class UpdateAnswerRequest {
+    private String answerText;
 }
