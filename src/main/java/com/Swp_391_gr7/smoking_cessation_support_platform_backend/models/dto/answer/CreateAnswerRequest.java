c73945b014f59fe5516f70ba6fd7e9fdcf3757f4
Võ Hoàng Minh <minh7n3@gmail.com>
2025-06-23 03:46:00 +0700

    update CreateAnswerRequest

--- src/main/java/com/Swp_391_gr7/smoking_cessation_support_platform_backend/models/dto/answer/CreateAnswerRequest.java
+++ src/main/java/com/Swp_391_gr7/smoking_cessation_support_platform_backend/models/dto/answer/CreateAnswerRequest.java
@@ -1,4 +1,8 @@
 package com.Swp_391_gr7.smoking_cessation_support_platform_backend.models.dto.answer;
 
+import java.time.LocalDateTime;
+
 public class CreateAnswerRequest {
+    private String answerText;
+    private LocalDateTime createdAt;
 }
