b7c64c020ee1ec5f9f5820babadc09894ad22ef0
Võ Hoàng Minh <minh7n3@gmail.com>
2025-06-23 03:51:53 +0700

    add findByQuestion_Id in AnswerRepository

--- src/main/java/com/Swp_391_gr7/smoking_cessation_support_platform_backend/repositories/AnswerRepository.java
+++ src/main/java/com/Swp_391_gr7/smoking_cessation_support_platform_backend/repositories/AnswerRepository.java
@@ -1,4 +1,15 @@
 package com.Swp_391_gr7.smoking_cessation_support_platform_backend.repositories;
 
-public interface AnswerRepository {
+import com.Swp_391_gr7.smoking_cessation_support_platform_backend.models.entity.Answer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface AnswerRepository extends JpaRepository<Answer, UUID> {
+    // Tìm danh sách câu trả lời theo ID của câu hỏi
+    List<Answer> findByQuestion_Id(UUID questionId);
+
+
+
 }
