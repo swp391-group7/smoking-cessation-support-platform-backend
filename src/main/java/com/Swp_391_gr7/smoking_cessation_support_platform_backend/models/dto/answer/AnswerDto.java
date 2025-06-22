aeca92114d16981a9e00a7c90b7826da08050c79
Võ Hoàng Minh <minh7n3@gmail.com>
2025-06-23 03:44:01 +0700

    update AnswerDto

--- src/main/java/com/Swp_391_gr7/smoking_cessation_support_platform_backend/models/dto/answer/AnswerDto.java
+++ src/main/java/com/Swp_391_gr7/smoking_cessation_support_platform_backend/models/dto/answer/AnswerDto.java
@@ -1,4 +1,11 @@
 package com.Swp_391_gr7.smoking_cessation_support_platform_backend.models.dto.answer;
 
+import java.time.LocalDateTime;
+import java.util.UUID;
+
 public class AnswerDto {
+    private UUID id;
+    private UUID questionId;
+    private String answerText;
+    private LocalDateTime createdAt;
 }
