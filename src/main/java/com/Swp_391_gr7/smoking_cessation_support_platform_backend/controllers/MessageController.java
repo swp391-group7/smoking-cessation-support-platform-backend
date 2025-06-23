d06084f0d655847ade78cd68558fcc66afcbf38c
LTdoshit <tuannleeeee@gmail.com>
2025-06-24 03:18:09 +0700

    rework on message functions

--- src/main/java/com/Swp_391_gr7/smoking_cessation_support_platform_backend/controllers/MessageController.java
+++ src/main/java/com/Swp_391_gr7/smoking_cessation_support_platform_backend/controllers/MessageController.java
@@ -1,27 +1,54 @@
-// MessageController.java
 package com.Swp_391_gr7.smoking_cessation_support_platform_backend.controllers;
 
+import com.Swp_391_gr7.smoking_cessation_support_platform_backend.models.dto.chat.ChatMessageDto;
+import com.Swp_391_gr7.smoking_cessation_support_platform_backend.models.entity.ChatRoom;
 import com.Swp_391_gr7.smoking_cessation_support_platform_backend.models.entity.Message;
+import com.Swp_391_gr7.smoking_cessation_support_platform_backend.models.entity.User;
+import com.Swp_391_gr7.smoking_cessation_support_platform_backend.repositories.ChatRoomRepository;
 import com.Swp_391_gr7.smoking_cessation_support_platform_backend.services.message.MessageService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.messaging.handler.annotation.DestinationVariable;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("/messages")
+@Controller
 @RequiredArgsConstructor
+@CrossOrigin("http://localhost:5173")
+@RequestMapping("/messages")
 public class MessageController {
-    private final MessageService service;
 
+    private final MessageService messageService;
+    private ChatRoomRepository roomRepository;
+
+
+
+    //for sending and receiving messages
     @GetMapping("/room/{roomId}")
-    public List<Message> getByRoom(@PathVariable UUID roomId) {
-        return service.getMessagesByRoom(roomId);
+    public ResponseEntity<List<Message>> getRoomMessages(@PathVariable UUID roomId) {
+        return ResponseEntity.ok(messageService.getMessagesByRoom(roomId));
     }
 
-    @PostMapping
-    public Message send(@RequestBody Message message) {
-        return service.save(message);
+    @MessageMapping("/sendMessage/{roomId}")
+    @PostMapping("/sendMessage/{roomId}")
+    @SendTo("/topic/room/{roomId}")//subscribe
+    public ResponseEntity<Message> sendMessage(@PathVariable UUID roomId, @RequestBody ChatMessageDto messageDto) {
+        String principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal().toString();
+        UUID currentUserId = UUID.fromString(principal);
+        Message message = Message.builder()
+                .chatRoom(ChatRoom.builder().id(roomId).build())
+                .sender(User.builder().id(currentUserId).build())
+                .content(messageDto.getContent())
+                .build();
+
+        return ResponseEntity.ok(messageService.save(message));
     }
 }
